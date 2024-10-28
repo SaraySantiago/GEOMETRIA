6d60792d3642d5b02a213586219b55e0132adf5e
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-28 10:54:22 +0100

    .

--- Circulo.py
+++ Circulo.py
@@ -4,14 +4,18 @@ from Figura import figura
 
 class circulo(figura): # Los circulos SON figuras. Tiene ya lo mismo que figura. 
     
-    def __init__(self, dimensions=...) -> None:
+    __color = None
+    
+    def __init__(self, dimensions=...,color=None) -> None:
         super().__init__(dimensions)
 
 
 if __name__ == "__main__": 
     
+    f0 = figura() 
+    print (f0.getDimensiones())
     c0 = circulo()
-    print(c0)
+    print(c0.getDimensiones())
 
-    
+
     
